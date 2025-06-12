e7f212ee14ebe3a18c06a873ca7ca747f816124a
Anthony <22-38317@g.batstate-u.edu.ph>
2025-06-12 22:08:12 +0800

    boolean 0 to true

--- config/migrate_erd.php
+++ config/migrate_erd.php
@@ -30,7 +30,7 @@ runQuery("CREATE TABLE users (
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    is_member BOOLEAN DEFAULT 0,
+    is_member BOOLEAN DEFAULT TRUE,
     date_joined TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )", "Users table");
 
